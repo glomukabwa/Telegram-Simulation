59198729f9535887b738dc7a9612f899cb558870
mukabwag <mukabwaglo@gmail.com>
2025-11-15 12:02:18 +0300

    List of Messages. Sill needs formatting

--- app/src/main/java/com/example/telegramsimulation/MainActivity.kt
+++ app/src/main/java/com/example/telegramsimulation/MainActivity.kt
@@ -10,10 +10,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -21,6 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,7 +50,9 @@ data class MessageContent(val author: String, val text: String)
 
 @Composable
 fun OneMessage(msg: MessageContent){
-    Row(modifier = Modifier.padding(all = 10.dp)){
+    Row(modifier = Modifier
+        .padding(all = 10.dp)
+        .fillMaxWidth()){
         Image(
             painter = painterResource(R.drawable.black_image),
             contentDescription = "Profile icon",
@@ -58,7 +65,27 @@ fun OneMessage(msg: MessageContent){
 
         Column {
             Text(text = msg.author)
-            Text(text = msg.text)
+            Text(text = msg.text,
+                modifier = Modifier.padding(bottom = 10.dp))
+
+            Divider(color = Color.Gray, thickness = 0.5.dp)
+            //A divider is just a thin line
+            /*Color.Gray is built into Jetpack Compose (and Kotlin/Android) as part of the standard Color class.
+            You don’t have to define it in your theme unless you want a custom shade.
+            Color in Compose has a bunch of predefined colors, including:
+            Color.Black
+            Color.White
+            Color.Red
+            Color.Green
+            Color.Blue
+            Color.Gray
+            Color.LightGray
+            Color.DarkGray
+            Color.Cyan
+            Color.Magenta
+            Color.Yellow
+            Color.Transparent
+            */
         }
     }
 }
@@ -69,6 +96,25 @@ fun previewOneMessage(){
     TelegramSimulationTheme {
         Surface(modifier = Modifier.fillMaxSize()){
             OneMessage(MessageContent("Gloria", "Hey Girllllll"))
+        }
+    }
+}
+
+@Composable
+fun allMessages(messages: List<MessageContent>){
+    LazyColumn {
+        items(messages){message ->
+            OneMessage(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun previewMessages(){
+    TelegramSimulationTheme {
+        Surface{
+            allMessages(SampleData.conversationSample)
         }
     }
 }
---
+++ app/src/main/java/com/example/telegramsimulation/SampleData.kt
@@ -0,0 +1,75 @@
+package com.example.telegramsimulation
+
+/**
+ * SampleData for Jetpack Compose Tutorial 
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        MessageContent(
+            "Lexi",
+            "Test...Test...Test..."
+        ),
+        MessageContent(
+            "Anna",
+            """List of Android versions:
+            |Android KitKat (API 19)
+            |Android Lollipop (API 21)
+            |Android Marshmallow (API 23)
+            |Android Nougat (API 24)
+            |Android Oreo (API 26)
+            |Android Pie (API 28)
+            |Android 10 (API 29)
+            |Android 11 (API 30)
+            |Android 12 (API 31)""".trim()
+        ),
+        MessageContent(
+            "Fred",
+            """I think Kotlin is my favorite programming language.
+            |It's so much fun!""".trim()
+        ),
+        MessageContent(
+            "Priscilla",
+            "Searching for alternatives to XML layouts..."
+        ),
+        MessageContent(
+            "Tina",
+            """Hey, take a look at Jetpack Compose, it's great!
+            |It's the Android's modern toolkit for building native UI.
+            |It simplifies and accelerates UI development on Android.
+            |Less code, powerful tools, and intuitive Kotlin APIs :)""".trim()
+        ),
+        MessageContent(
+            "Liz",
+            "It's available from API 21+ :)"
+        ),
+        MessageContent(
+            "Tabitha",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        MessageContent(
+            "Elena",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        MessageContent(
+            "Naomi",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        MessageContent(
+            "Rex",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        MessageContent(
+            "Lincoln",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        MessageContent(
+            "Tara",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        MessageContent(
+            "Tidashe",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
