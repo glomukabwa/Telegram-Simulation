fa25d075f86d7f609962f5c20cec3e5d390244c9
mukabwag <mukabwaglo@gmail.com>
2026-06-12 11:52:16 +0300

    Added Surface

--- app/src/main/java/com/example/telegramsimulation/Contacts.kt
+++ app/src/main/java/com/example/telegramsimulation/Contacts.kt
@@ -1,21 +1,39 @@
 package com.example.telegramsimulation
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.telegramsimulation.ui.theme.TelegramSimulationTheme
 
 data class contactInfo(val fname: String, val pNumber: String, val profile: Int)
 
 @Composable
 fun oneContact(contact: contactInfo){
-    Row {
+    Row (
+        modifier = Modifier.padding(10.dp),
+        horizontalArrangement = Arrangement.spacedBy(15.dp)
+    ){
         Image(
             painter = painterResource(contact.profile),
-            contentDescription = "Profile picture"
+            contentDescription = "Profile picture",
+            modifier = Modifier
+                .size(55.dp)
+                .clip(CircleShape)
+
         )
 
         Column {
@@ -25,8 +43,21 @@ fun oneContact(contact: contactInfo){
     }
 }
 
+
+@Composable
+fun contactSurface(){
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        oneContact(contactInfo("Gloria", "0717292108", R.drawable.black_image))
+    }
+}
+
 @Preview
 @Composable
-fun previewOneContact(){
-    oneContact(contactInfo("Gloria", "0717292108", R.drawable.profile_icon))
+fun previewContactsSurface(){
+    TelegramSimulationTheme {
+        contactSurface()
+    }
 }
